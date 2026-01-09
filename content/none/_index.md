45dc8d8c030e6f61885060cd7396590b63bf3f80
lanius <mohammedno1@proton.me>
2026-01-09 20:39:31 +0200

    إضافة بوت تليجرام لنشر المقالات الجديدة

--- content/none/_index.md
+++
@@ -1,7 +0,0 @@
-+++
-title = "الكُنَّاشَةُ"
-sort_by = "date"
-template = "posts.html"
-page_template = "post.html"
-
-+++
--- public/js/menu.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector(".menu");
-let plus = document.querySelector(".menu .plus");
-let links = document.querySelector(".links");
-menu.addEventListener("click", () => {
-   links.classList.toggle('expand');
-   plus.classList.toggle('active');
-
-})
